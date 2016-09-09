93b4ddcbfac5c2a07742fee2f207f1baa82762fd
Yongbo Li <maxleebo@gmail.com>
2016-09-08 21:15:09 -0400

    commit lab 02

---
+++ mean_median.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h> /* SIZE_MAX */
+#include <string.h>
+#include <math.h>
+
+void select_sort(int A[], size_t n)
+{
+	//Your code goes here!
+
+}
+
+int read_array(FILE *fp, int *p_A[])
+{
+	//Your code goes here!
+
+}
+
+void print_array(const int *A, int n)
+{
+	//Your code goes here!
+
+}
+
+double mean_array(const int *A, int n)
+{
+	//Your code goes here!
+
+}
+
+double median_array(const int *A, int n)
+{
+	//Your code goes here!
+
+}
