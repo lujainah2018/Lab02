6a1190ffe221d29fbe11ab35632abadc1847f829
maotongxu <maotongxu@gmail.com>
2018-01-31 14:55:46 -0500

    Update mean_median.c

--- mean_median.c
+++ mean_median.c
@@ -12,8 +12,53 @@ void select_sort(int A[], size_t n)
 
 int read_array(FILE *fp, int *p_A[])
 {
-	//Your code goes here!
+	ssize_t readCount = 0;
+	size_t N = 2, s = sizeof(int);
+	size_t max_elms = SIZE_MAX / s;
+	int *A = NULL, *P, num;
 
+	*p_A = NULL;
+
+	for (;;) {
+
+		if (fscanf(fp, "%d", &num) == 1) {
+		}
+		else if (ferror(fp)) {
+			free(A);
+			return -1;
+		}
+		else if (feof(fp)) {
+			break;
+		}
+
+		if (A == NULL || readCount == N) {
+			N = N * 3 / 2;
+
+			if (N >= max_elms) {
+				N = max_elms;
+			}
+
+			size_t size = N * s;
+
+			P = realloc(A, size);
+
+			if (P == NULL) {
+				free(A);
+				return -1;
+			}
+
+			A = P;
+		}
+
+		A[readCount] = num;
+		readCount++;
+
+		if (readCount == max_elms)
+			break;
+	}
+
+	*p_A = A;
+	return readCount;
 }
 
 void print_array(const int *A, int n)
